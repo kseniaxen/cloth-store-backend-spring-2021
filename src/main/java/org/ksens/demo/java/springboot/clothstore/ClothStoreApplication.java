e951e12fbbd34d7a7c0ab1d6aa111dadc2a8b131
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2021-06-02 13:18:00 +0300

    Initial commit

---
+++ src/main/java/org/ksens/demo/java/springboot/clothstore/ClothStoreApplication.java
@@ -0,0 +1,13 @@
+package org.ksens.demo.java.springboot.clothstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ksens/demo/java/springboot/clothstore/ClothStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ksens.demo.java.springboot.clothstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
